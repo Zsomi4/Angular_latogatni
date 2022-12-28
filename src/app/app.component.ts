32d6689d88375090d76fa576b9c3adc1a9b6efe5
Zsomi4 <k72302872172@ktch.hu>
2022-12-28 08:38:52 +0100

    settlement interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+interface settlement {
+  name: string;
+  county: string;
+  area: number;
+  population: number;
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
