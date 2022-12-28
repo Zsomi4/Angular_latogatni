8c0f5e6253ab3eda1d961cbf885319584e3f548b
Zsomi4 <k72302872172@ktch.hu>
2022-12-28 08:51:00 +0100

    Adatok feltöltése a settlementArray tömbe.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,50 @@ interface settlement {
   population: number;
 }
 
-
+const settlementArray:settlement[] = [
+  {
+    name: "Székesfehérvár",
+    county: "Fejér",
+    area: 170.89,
+    population: 95545
+  },
+  {
+    name: "Szombathely",
+    county: "Vas",
+    area: 97.5,
+    population: 78324
+  },
+  {
+    name: "Sopron",
+    county: "Győr-Moson-Sopron",
+    area: 169.04,
+    population: 62900
+  },
+  {
+    name: "Zalaegerszeg",
+    county: "Zala",
+    area: 102.42,
+    population: 55470
+  },
+  {
+    name: "Sárvár",
+    county: "Vas",
+    area: 64.65,
+    population: 15072
+  },
+  {
+    name: "Lenti",
+    county: "Zala",
+    area: 73.79,
+    population: 7348
+  },
+  {
+    name: "Zalakaros",
+    county: "Zala",
+    area: 17.17,
+    population: 1936
+  }
+]
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
